1535cc58bc93bb1e257b31153ab89f36f4ead8b1
Rahat Alom <rahat.alom@orgvue.com>
2023-03-27 18:33:46 +0100

    refactor and correct pawn movement

--- src/ChessBoard/utils.ts
+++ src/ChessBoard/utils.ts
@@ -1,44 +1,52 @@
 import { letters } from "../constants";
 import { ChessPieceType } from "../types";
 
-export const getPossibleMoves = (positionObject: Record<string,ChessPieceType>,sourceId: string) => {
-    const movingPiece = positionObject[sourceId];
-    if (movingPiece?.endsWith("Pawn")) {
-      if (movingPiece.startsWith("W")) {
-        const column = sourceId[0];
-        const rank = +sourceId[1];
-        let possibleMoves: string[] = [];
-        possibleMoves.push(column + (rank + 1));
-        if (rank === 2) {
-          possibleMoves.push(column + (rank + 2));
-        }
-        const index = letters.indexOf(column);
-        const captureSquares = [
-          letters[index + 1] + (rank + 1),
-          letters[index - 1] + (rank + 1),
-        ].filter((sqr) => sqr && positionObject[sqr]?.startsWith("B"));
+export const getPossibleMoves = (
+  positionObject: Record<string, ChessPieceType>,
+  sourceId: string
+) => {
+  const movingPiece = positionObject[sourceId];
+  let possibleMoves: string[] = [];
+  const column = sourceId[0];
+  const rank = +sourceId[1];
 
-        possibleMoves = [...possibleMoves, ...captureSquares];
-
-        return possibleMoves;
+  if (movingPiece?.endsWith("Pawn")) {
+    if (movingPiece.startsWith("W")) {
+      possibleMoves.push(column + (rank + 1));
+      if (rank === 2) {
+        possibleMoves.push(column + (rank + 2));
       }
-      if (movingPiece.startsWith("B")) {
-        const column = sourceId[0];
-        const rank = +sourceId[1];
-        let possibleMoves: string[] = [];
-        possibleMoves.push(column + (rank - 1));
-        if (rank === 7) {
-          possibleMoves.push(column + (rank - 2));
-        }
-        const index = letters.indexOf(column);
-        const captureSquares = [
-          letters[index + 1] + (rank - 1),
-          letters[index - 1] + (rank - 1),
-        ].filter((sqr) => sqr && positionObject[sqr]?.startsWith("W"));
+      possibleMoves = possibleMoves.filter((sqr) =>
+        !positionObject[sqr]?.startsWith("B")
+      );
+      const index = letters.indexOf(column);
+      const captureSquares = [
+        letters[index + 1] + (rank + 1),
+        letters[index - 1] + (rank + 1),
+      ].filter((sqr) => sqr && positionObject[sqr]?.startsWith("B"));
 
-        possibleMoves = [...possibleMoves, ...captureSquares];
+      possibleMoves = [...possibleMoves, ...captureSquares];
 
-        return possibleMoves;
+      return possibleMoves;
+    }
+    if (movingPiece.startsWith("B")) {
+      possibleMoves.push(column + (rank - 1));
+      if (rank === 7) {
+        possibleMoves.push(column + (rank - 2));
       }
+      possibleMoves = possibleMoves.filter((sqr) =>
+        !positionObject[sqr]?.startsWith("W")
+      );
+
+      const index = letters.indexOf(column);
+      const captureSquares = [
+        letters[index + 1] + (rank - 1),
+        letters[index - 1] + (rank - 1),
+      ].filter((sqr) => sqr && positionObject[sqr]?.startsWith("W"));
+
+      possibleMoves = [...possibleMoves, ...captureSquares];
+
+      return possibleMoves;
     }
   }
+};
